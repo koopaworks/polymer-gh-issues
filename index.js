91ad42ac7a29d45051302981c8ec0f5c2ba185e8
Scott Sanders <scott@stonecobra.com>
2016-06-07 12:46:39 -0700

    yo polymer:seed build-up

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use('/components/gh-issues', express.static( __dirname));
+app.use('/components', express.static(__dirname + '/bower_components'));
+
+app.get('/hello', function (req, res) {
+  res.status(200).send('Hello, world!');
+});
+
+var server = app.listen(process.env.PORT || '8080', function () {
+  console.log('App listening on port %s', server.address().port);
+});
