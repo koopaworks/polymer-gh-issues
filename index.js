c565c3635428128d6c49c77cb175b6199522c670
Scott Sanders <scott@stonecobra.com>
2016-06-07 13:01:56 -0700

    auto redirect to example page

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ var app = express();
 app.use('/components/gh-issues', express.static( __dirname));
 app.use('/components', express.static(__dirname + '/bower_components'));
 
+app.get('/', function(req, res){
+  res.redirect('/components/gh-issues/');
+});
+
 app.get('/hello', function (req, res) {
   res.status(200).send('Hello, world!');
 });
